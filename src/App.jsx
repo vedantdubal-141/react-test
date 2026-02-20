7f20ee17c9f8c507aa4c8593d3335b3e08f7c760
Vedant Dubal <vedantdubal141@gmail.com>
2026-02-20 19:39:20 +0900

    chore: setup github pages deployment with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
 import Students from './pages/Students';
